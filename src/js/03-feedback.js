d99f54f5780f9923d9708e314acaccafe0183969
FairySaikiNoKysyo <michaelevgeniymyers@gmail.com>
2023-09-12 12:14:18 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,31 +1,32 @@
 import throttle from 'lodash.throttle';
 
-const form = document.querySelector('.js-form');
-const email = document.querySelector('input[name="email"]');
-const message = document.querySelector('textarea[name="message"]');
-
+const elements = {
+  form: document.querySelector('.js-form'),
+  email: document.querySelector('input[name="email"]'),
+  message: document.querySelector('textarea[name="message"]'),
+};
 const KEY_LS = 'feedback-form-state';
 
-form.addEventListener('input', throttle(handlerInput, 500));
-form.addEventListener('submit', handlerSubmit);
+elements.form.addEventListener('input', throttle(handlerInput, 500));
+elements.form.addEventListener('submit', handlerSubmit);
 
 function handlerInput(evt) {
-  const value = { email: email.value, message: message.value };
+  const value = { email: elements.email.value, message: elements.message.value };
   localStorage.setItem(KEY_LS, JSON.stringify(value));
 }
 
 function handlerSubmit(evt) {
   evt.preventDefault();
-  if (email.value === '' || message.value === '') {
+  if (elements.email.value === '' || elements.message.value === '') {
     return alert('Заповніть усі поля, будь-ласка!');
   }
   console.log(JSON.parse(localStorage.getItem(KEY_LS) ?? {}));
-  form.reset();
+  elements.form.reset();
   localStorage.removeItem(KEY_LS);
 }
 
 if (JSON.parse(localStorage.getItem(KEY_LS)) !== '') {
   const savedValue = JSON.parse(localStorage.getItem(KEY_LS)) ?? {};
-  email.value = savedValue.email ?? '';
-  message.value = savedValue.message ?? '';
+  elements.email.value = savedValue.email ?? '';
+  elements.message.value = savedValue.message ?? '';
 }
